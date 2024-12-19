910f0453fd5f8f187341e29b399c9575a1aadf77
vanquangthai <thaivanquang996@gmail.com>
2024-12-19 11:55:32 +0100

    Add function save excel file with date

--- Sensor/ReadHeatMap.py
+++ Sensor/ReadHeatMap.py
@@ -2,10 +2,13 @@ import serial
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pandas as pd
+from datetime import datetime
+import os
 
 # Configure serial port (adjust these parameters to your setup)
 SERIAL_PORT = 'COM10'  # Replace with your serial port
-BAUD_RATE = 115200               # Replace with your baud rate
+BAUD_RATE = 115200     # Replace with your baud rate
 
 # Initialize serial connection
 ser = serial.Serial(SERIAL_PORT, BAUD_RATE)
@@ -39,9 +42,23 @@ while len(data) < NUM_COLUMNS * NUM_ROWS:
 # Convert the data into a numpy array and reshape it into a 32x24 matrix
 data_matrix = np.array(data[:NUM_COLUMNS * NUM_ROWS]).reshape(NUM_ROWS, NUM_COLUMNS)
 
+# Create the 'data' folder if it doesn't exist
+output_folder = 'data'
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
 
+# Save the matrix as an Excel file
+current_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')  # Get current datetime as a string
+output_file = os.path.join(output_folder, f'{current_time}.xlsx')  # Construct the file path
+
+# Convert the numpy matrix to a DataFrame
+df = pd.DataFrame(data_matrix)
+
+# Save the DataFrame to an Excel file
+df.to_excel(output_file, index=False, header=False)
+
+print(f"Matrix saved as Excel file: {output_file}")
 # Plot the heatmap using plt.imshow
-
 plt.imshow(data_matrix, cmap='hot', interpolation='nearest')
 
 # Add a color bar with label
@@ -55,18 +72,7 @@ plt.ylabel('Height (Pixels)')
 # Display the heatmap
 plt.show()
 
-
-# # Create the heatmap
-# plt.figure(figsize=(10, 8))  # Adjust the size of the plot
-# sns.heatmap(data_matrix, annot=True, cmap='YlGnBu', cbar=True, fmt='.1f')
-
-# # Add labels and title
-# plt.title('Temperature Heatmap')
-# plt.xlabel('Column Index')
-# plt.ylabel('Row Index')
-
-# # Show the heatmap
-# plt.show()
-
 # Close the serial connection
 ser.close()
+
+
